12e27f35138ce166a1f51db92e93aee75183276d
viedell <49792489+viedell@users.noreply.github.com>
2025-03-16 22:54:26 +0700

    Create 7.py

---
+++ 7.py
@@ -0,0 +1,12 @@
+def bmi_calculator():
+    weight = float(input("Enter weight (kg): "))
+    height = float(input("Enter height (m): "))
+    bmi = weight / (height * height)
+    if bmi < 18.5:
+        print("Underweight")
+    elif bmi < 25:
+        print("Normal")
+    elif bmi < 30:
+        print("Overweight")
+    else:
+        print("Obese")
