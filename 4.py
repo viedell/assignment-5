7284ccc67f979e2ad93a28756a367360d56b17c8
viedell <49792489+viedell@users.noreply.github.com>
2025-03-16 22:53:46 +0700

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,5 @@
+def largest_number():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    c = int(input("Enter third number: "))
+    print("Largest number is:", max(a, b, c))
