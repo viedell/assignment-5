3d28538f017afbadbdb7151fda64bd2d9ec6c400
viedell <49792489+viedell@users.noreply.github.com>
2025-03-17 10:02:00 +0700

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,14 +1,13 @@
-def calculator():
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-    op = input("Enter operator (+, -, *, /): ")
-    if op == '+':
-        print(num1 + num2)
-    elif op == '-':
-        print(num1 - num2)
-    elif op == '*':
-        print(num1 * num2)
-    elif op == '/' and num2 != 0:
-        print(num1 / num2)
-    else:
-        print("Invalid operation or division by zero.")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+op = input("Enter operator (+, -, *, /): ")
+if op == '+':
+    print(num1 + num2)
+elif op == '-':
+    print(num1 - num2)
+elif op == '*':
+    print(num1 * num2)
+elif op == '/' and num2 != 0:
+    print(num1 / num2)
+else:
+    print("Invalid operation or division by zero.")
