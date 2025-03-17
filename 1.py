8cca3c26b69d8384957023b955527327a74869a6
viedell <49792489+viedell@users.noreply.github.com>
2025-03-17 09:57:07 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,5 @@
-def check_number():
-    num = float(input("Enter a number: "))
-    if num > 0:
-        print("Positive")
-    else:
-        print("Negative")
+num = float(input("Enter a number: "))
+if num > 0:
+    print("Positive")
+else:
+    print("Negative")
