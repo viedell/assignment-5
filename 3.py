295c37cc029e56772ca47b47f4ba30ffe0b58bbc
viedell <49792489+viedell@users.noreply.github.com>
2025-03-17 10:00:56 +0700

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,12 +1,11 @@
-def grade_score():
-    score = int(input("Enter test score: "))
-    if score >= 80:
-        print("Grade: A")
-    elif score >= 70:
-        print("Grade: B")
-    elif score >= 60:
-        print("Grade: C")
-    elif score >= 50:
-        print("Grade: D")
-    else:
-        print("Grade: E")
+score = int(input("Enter test score: "))
+if score >= 80:
+    print("Grade: A")
+elif score >= 70:
+    print("Grade: B")
+elif score >= 60:
+    print("Grade: C")
+elif score >= 50:
+    print("Grade: D")
+else:
+    print("Grade: E")
