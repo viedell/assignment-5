66c61d3501730ab7727616ed9268244f178f078f
viedell <49792489+viedell@users.noreply.github.com>
2025-03-17 10:00:25 +0700

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,6 +1,5 @@
-def even_or_odd():
-    num = int(input("Enter a number: "))
-    if num % 2 == 0:
-        print("Even")
-    else:
-        print("Odd")
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
