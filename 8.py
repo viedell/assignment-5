e1657af0dbfe7eb0e4338943cd0b06798448784f
viedell <49792489+viedell@users.noreply.github.com>
2025-03-17 10:03:42 +0700

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,13 +1,12 @@
-def triangle_type():
-    a = int(input("Enter first side: "))
-    b = int(input("Enter second side: "))
-    c = int(input("Enter third side: "))
-    if a + b > c and a + c > b and b + c > a:
-        if a == b == c:
-            print("Equilateral Triangle")
-        elif a == b or a == c or b == c:
-            print("Isosceles Triangle")
-        else:
-            print("Scalene Triangle")
-    else:
-        print("Not a valid triangle")
+a = int(input("Enter first side: "))
+b = int(input("Enter second side: "))
+c = int(input("Enter third side: "))
+if a + b > c and a + c > b and b + c > a:
+    if a == b == c:
+        print("Equilateral Triangle")
+    elif a == b or a == c or b == c:
+        print("Isosceles Triangle")
+else:
+    print("Scalene Triangle")
+else:
+    print("Not a valid triangle")
